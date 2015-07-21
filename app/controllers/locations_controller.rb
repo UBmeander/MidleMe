a8c40c33bedbfe73ccd712ed961719bb37212831
Joshua C. Eubanks <ubmeander@gmail.com>
2015-07-21 18:56:29 -0400

    fix set_location method; delete application.js

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -63,7 +63,9 @@ class LocationsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-   
+    def set_location
+      @location = Location.find(params[:id])
+    end
    
     # Never trust parameters from the scary internet, only allow the white list through.
     def location_params
